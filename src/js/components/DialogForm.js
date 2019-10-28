8ef4ad5c62d0f9772cf375b597e9dfc500847edb
Балтабек Асхат Балтабекұлы <Askhat.Baltabek@kaspi.kz>
2019-10-28 17:34:21 +0600

    merge fix

--- src/js/components/DialogForm.js
+++ src/js/components/DialogForm.js
@@ -8,11 +8,48 @@ moment.updateLocale('en', {
     },
 })
 
+const ConnectedForm = styled.form`
+    
+`
+const SInput = styled.input`
+    width: 100%;
+    margin-bottom: 10px;
+`
+const STextarea = styled.textarea`
+    max-width: 100%
+    min-width: 100%;
+    min-height: 200px;
+`
+
 class DialogForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: '',
+            description: '',
+        }
+    }
+
+    handleChange = (event)=> {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        
+    }
 
     render() {
+        const state = this.state;
         return (
-            <input type='text' />
+            <ConnectedForm onSubmit={this.handleSubmit}>
+                <label htmlFor="title">Title:</label>
+                <SInput type="text" id="title" name="title" onChange={this.handleChange} value={state.title} />
+                <label htmlFor="description">Description:</label>
+                <STextarea name="description" id="description" onChange={this.handleChange} defaultValue={state.description}></STextarea>
+            </ConnectedForm>
         )
     }
 }
